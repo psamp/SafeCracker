cb33f5c08d71428259c7afcb2e925752bf636c4c
Princess Sampson <ps@kingonomics.com>
2016-08-10 17:25:44 -0400

    Allow the user to enter their name and get a greeting in return

--- SafeCracker/main.c
+++ SafeCracker/main.c
@@ -7,9 +7,34 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+int takeInAString(char*);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    char user[30] = {'\0'};
+    int scannedUsersNameSuccesfully = -1;
+    
+    printf("Welcome to SafeCracker! If you guess the code to an evil billionaire's safe, everything inside is yours!\nSo we know who to make the check out to in the event you guess correctly, please tell us your name.\n");
+    printf("Enter name:\n");
+    
+    while (scannedUsersNameSuccesfully != 1) {
+        scannedUsersNameSuccesfully = takeInAString(user);
+    }
+    
+    printf("Nice to meet you, %s\n", user);
+    
     return 0;
+}
+
+
+int takeInAString(char* destinationVariable) {
+    
+    
+    fpurge(stdin);
+    int itemsScanned = scanf("%[^\n]s", destinationVariable);
+    
+    
+    return itemsScanned;
 }
