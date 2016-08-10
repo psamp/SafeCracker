b2eddcd931864069172c6f44417807deb7e1a083
Princess Sampson <ps@kingonomics.com>
2016-08-10 15:40:54 -0400

    Initial Commit

---
+++ SafeCracker/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SafeCracker
+//
+//  Created by Princess Sampson on 8/10/16.
+//  Copyright © 2016 Princess Sampson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
