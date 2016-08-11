90ffb12f9ea71b362b5f6739f804745c84434d02
Princess Sampson <ps@kingonomics.com>
2016-08-11 03:35:45 -0400

    Rip out old game logic and implement the most basic requirements and the two star challenge

--- SafeCracker/main.c
+++ SafeCracker/main.c
@@ -8,18 +8,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 int takeInAString(char*);
 void getNewCombination(int* combination);
-void guessCombination(int* guessedCombination);
-void takeInCombonationGuess(int* guesses);
+void takeInCombinationGuess(int* guessedCombination);
+bool compareIntArrays(int* a, int* b, int lengthOfArrays);
+void gameplay(void);
 
 int main(int argc, const char * argv[]) {
     
     char user[30] = {'\0'};
     int scannedUsersNameSuccesfully = -1;
     
-    printf("Welcome to SafeCracker! If you guess the code to an evil billionaire's safe, all the riches within are yours\nYou have 3 chances to guess correctly.\nSo we know who to make the check out to in the event you guess correctly, please tell us your name.\n\n");
+    printf("Welcome to SafeCracker! If you guess the code to an evil billionaire's safe, all the riches within are yours.\nYou have 3 chances.\nSo we know who to make the check out to in the event you guess correctly, please tell us your name.\n\n");
     printf("Enter name:\n\n");
     
     while (scannedUsersNameSuccesfully != 1) {
@@ -28,52 +31,87 @@ int main(int argc, const char * argv[]) {
     
     printf("Nice to meet you, %s.\n\n", user);
     
+    gameplay();
+    
+    return 0;
+}
+
+void gameplay() {
     int combination[4] = {[0 ... 3] = -1};
+    
+    int timesUserHasGuessed = 0;
+    
+    int guessedCombination[4] = {[0 ... 3] = -1};
+    int digitsGuessedThatAreInCombination[4] = {[0 ... 3] = -1};
+    
+    bool guessing = true;
+    
     getNewCombination(combination);
-    char combinationString[4] = {[0 ... 3] = -1};
     
+    printf("Combination: ");
     for (int i = 0; i < 4; i++) {
-        combinationString[i] = combination[i] + '0';
+        printf("%d", combination[i]);
     }
+    printf("\n\n");
     
-    int timesGuessed = 0;
-    int guessedCombination[4] = {0};
-    
-    int correctGuesses[4] = {[0 ... 3] = -1};
-    int howManyCorrectGuesses = 0;
-    
-    
-    while (timesGuessed <= 2) {
+    while (guessing) {
         
-        printf("COMBINATION: %s\n\n", combinationString);
-        
-        guessCombination(guessedCombination);
-        
-        /*if ((char) guessedCombination[0] == 'p') {
-         
-         }*/
+        takeInCombinationGuess(guessedCombination);
         
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                if(guessedCombination[i] == combination[j]) {
-                    correctGuesses[i] = guessedCombination[i];
+                if (guessedCombination[i] == combination[j]) {
+                    digitsGuessedThatAreInCombination[i] = guessedCombination[i];
                 }
             }
         }
         
-        timesGuessed++;
+        if (compareIntArrays(guessedCombination, combination, 4) == true) {
+            
+            printf("You won! You're rich!\n\n");
+            guessing = false;
+            
+        } else {
+            
+            printf("You tried! These are the digits you guessed correctly, and they feature somewhere in the safe combo.\nBut not nessasarily the same amount of times, or in the same order.\n\n");
+            
+            for (int i = 0; i < 4; i++) {
+                if (digitsGuessedThatAreInCombination[i] != -1) {
+                    printf("%d\n", digitsGuessedThatAreInCombination[i]);
+                }
+            }
+            
+            char input = '\0';
+            printf("To guess again, type \"y\". To quit, type anything else.\nEnter:\n\n");
+            fpurge(stdin);
+            int itemsScanned = takeInAString(&input);
+            
+            if (itemsScanned == 1 && input == 'y') {
+                timesUserHasGuessed = 0;
+            } else {
+                guessing = false;
+            }
+        }
+        
+        timesUserHasGuessed++;
     }
+}
+
+bool compareIntArrays(int* a, int* b, int lengthOfArrays) {
+    bool rtn = false;
+    int mactchingValuesAtEachIndex = 0;
     
-    for (int i = 0; i < 4; i++) {
-        if(correctGuesses[i] != -1) {
-            howManyCorrectGuesses++;
+    for (int i = 0; i < lengthOfArrays; i++) {
+        if (a[i] == b[i]) {
+            mactchingValuesAtEachIndex++;
         }
     }
     
-    printf("COMBONATION: %s\nYou guessed %d/4 digits! You're rich!\n", combinationString, howManyCorrectGuesses);
+    if(lengthOfArrays == mactchingValuesAtEachIndex) {
+        rtn = true;
+    }
     
-    
-    return 0;
+    return rtn;
 }
 
 
@@ -86,13 +124,13 @@ int takeInAString(char* destinationVariable) {
 
 void getNewCombination(int* combination){
     
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 4; i++) {
         combination[i] = arc4random_uniform(4);
     }
     
 }
 
-void guessCombination(int* guessedCombination) {
+void takeInCombinationGuess(int* guessedCombination) {
     int tookInGuessesSuccessfully = -1;
     
     while (tookInGuessesSuccessfully != 4) {
